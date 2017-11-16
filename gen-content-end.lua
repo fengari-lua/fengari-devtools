aedbce2b750f5fb3aad75dc9abe1961d94ad9913
Benoit Giannangeli <giann008@gmail.com>
2017-11-16 15:39:58 +0100

    content-end.js generated to insert fengari-web if it's not present
    TODO: more elegant way to do that...

---
+++ gen-content-end.lua
@@ -0,0 +1,15 @@
+local f = io.open("node_modules/fengari-web/dist/fengari-web.js", "r")
+local fengariWeb = f:read("*all"):gsub("`", ""):gsub("\\","\\\\")
+f:close()
+
+f = io.open("content-end.js", "w")
+f:write([[
+    (function () {
+        let script = document.createElement('script');
+        script.textContent = `]] .. fengariWeb .. [[`;
+        document.documentElement.appendChild(script);
+        script.parentNode.removeChild(script);
+    })()
+]])
+f:close()
+
